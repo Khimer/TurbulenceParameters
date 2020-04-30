b5cb0069170fbe95ecb75838fa95fab8358a5cac
Rostislav <zenigeba77@gmail.com>
2020-04-30 21:36:37 +0700

    Added several methods of the Service class, as well as some tests

--- TurbulenceParameters/Service.py
+++ TurbulenceParameters/Service.py
@@ -1,5 +1,74 @@
+from TurbulenceParameters.BytesParser import *
+from TurbulenceParameters.DataDownloader import *
+from TurbulenceParameters.ParametersCalculator import *
+from TurbulenceParameters.CsvWriter import *
+from TurbulenceParameters.CsvReader import *
+from TurbulenceParameters.DataAggregator import *
 
 
-class Service():
+class Service:
     def __init__(self):
         self.data = 0
+        self.connection = 0
+        self.availableTimeStart = 0
+        self.availableTimeEnd = 0
+        self.parser = BytesParser()
+        self.reader = CsvReader()
+        self.writer = CsvWriter()
+        self.calculator = ParametersCalculator()
+        self.aggregator = DataAggregator()
+        self.downloader = DataDownloader()
+
+    def check_connection_and_time_range(self):
+        def get_time(index):
+            path = 'http://amk030.imces.ru/meteodata/AMK_030_BIN/'
+            answer = requests.get(path)
+            pattern = r"(href=\"[\d\w.%]*[\/]\")"  # Для папок
+            folders = re.findall(pattern, answer.text)
+            position = folders[index][6:-1]
+            position = path + position
+            if index == 0:
+                answer = requests.get(position)
+                folders = re.findall(pattern, answer.text)
+                print(folders)
+                position = position + folders[index][6:-2]
+            answer = requests.get(position)
+            pattern = r"(href=\"[\d\w.]*B\")"  # для файлов
+            files = re.findall(pattern, answer.text)
+            file = files[index][6:-1]
+            time_date = datetime.strptime(
+                file[2:4] + '.' + file[:2] + '.20' + file[-3:-1] + ' ' + file[4:6] + ':' + file[6:8], '%d.%m.%Y %H:%M')
+            return time_date
+
+        if requests.get('http://amk030.imces.ru/meteodata/AMK_030_BIN/').status_code == 200:
+            self.connection = 1
+            self.availableTimeStart = get_time(0)
+            self.availableTimeEnd = get_time(-1)
+
+    def set_time_range(self, time_range):
+        self.downloader.start_datetime = time_range[0]
+        self.downloader.end_datetime = time_range[1]
+
+    def set_constants(self, constants):
+        self.calculator.constants = constants
+
+    def set_amount_aggregated_data(self, amount_aggregated_data):
+        self.aggregator.amountAggregatedData = amount_aggregated_data
+
+    def get_list_files(self):
+        pass
+
+    def download_data(self):
+        pass
+
+    def aggregate_data(self, data):
+        pass
+
+    def save_data(self, data, selected_time_range):
+        pass
+
+    def read_data(self, name_file):
+        pass
+
+    def calculate_turbulence_parameters(self):
+        pass
---
+++ TurbulenceParameters/utest.py
@@ -0,0 +1,110 @@
+import unittest
+from TurbulenceParameters.BytesParser import *
+from TurbulenceParameters.DataDownloader import *
+from TurbulenceParameters.ParametersCalculator import *
+from TurbulenceParameters.CsvWriter import *
+from TurbulenceParameters.CsvReader import *
+from TurbulenceParameters.DataAggregator import *
+
+
+# class DownloaderTest(unittest.TestCase):
+#
+#     def test_download(self):
+#         downloader = DataDownloader()
+#         downloader.start_datetime = datetime.strptime('15.08.2018 15:00', '%d.%m.%Y %H:%M')
+#         downloader.end_datetime = datetime.strptime('15.08.2018 16:00', '%d.%m.%Y %H:%M')
+#         downloader.download()
+#         self.assertGreater(len(downloader.binary_data), 0)
+#
+# class ParserTest(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.downloader = DataDownloader()
+#         self.downloader.start_datetime = datetime.strptime('15.08.2018 15:00', '%d.%m.%Y %H:%M')
+#         self.downloader.end_datetime = datetime.strptime('15.08.2018 16:00', '%d.%m.%Y %H:%M')
+#         self.downloader.download()
+#
+#     def test_parse(self):
+#         parser = BytesParser()
+#         data = parser.parse(self.downloader.binary_data[0])
+#         for binary_file in self.downloader.binary_data[1:]:
+#             data = np.append(data, parser.parse(binary_file), axis=1)
+#         self.assertEqual(data.shape[0], 7)
+#
+#
+# class ReaderTest(unittest.TestCase):
+#
+#     def test_read(self):
+#         reader = CsvReader()
+#         self.assertEqual(reader.read('testfile').shape[0], 7)
+#
+#
+# class WriterTest(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.reader = CsvReader()
+#         self.data = self.reader.read('testfile')
+#
+#     def test_write(self):
+#         writer = CsvWriter()
+#         writer.save_csv(self.data, 'new_file')
+#         self.assertIn('new_file', os.listdir(path="."))
+#
+#
+# class AggregatorTest(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.reader = CsvReader()
+#         self.data = self.reader.read('testfile')
+#
+#     def test_aggregate(self):
+#         aggregator = DataAggregator()
+#         aggregated_data = aggregator.aggregate(self.data)
+#         self.assertEqual(aggregated_data.shape[1], self.data.shape[1]//5)
+#
+#     class CalculatorTest(unittest.TestCase):
+#
+#         def setUp(self):
+#             self.reader = CsvReader()
+#             self.data = self.reader.read('testfile')
+#
+#         def test_calculate(self):
+#             calculator = ParametersCalculator()
+#             self.assertGreater(len(calculator.calculate(self.data)), 0)
+
+class CommonTest(unittest.TestCase):
+
+    def test_all(self):
+        downloader = DataDownloader()
+        downloader.start_datetime = datetime.strptime('15.08.2018 15:00', '%d.%m.%Y %H:%M')
+        downloader.end_datetime = datetime.strptime('15.08.2018 16:00', '%d.%m.%Y %H:%M')
+        downloader.download()
+        self.assertGreater(len(downloader.binary_data), 0)
+
+        parser = BytesParser()
+        data = parser.parse(downloader.binary_data[0])
+        for binary_file in downloader.binary_data[1:]:
+            data = np.append(data, parser.parse(binary_file), axis=1)
+        self.assertEqual(data.shape[0], 7)
+
+        aggregator = DataAggregator()
+        aggregated_data = aggregator.aggregate(data)
+        print(aggregated_data.shape, data.shape)
+        self.assertEqual(aggregated_data.shape[1], data.shape[1] // 100)
+
+        calculator = ParametersCalculator()
+        calculator.calculate(aggregated_data)
+        self.assertGreater(len(calculator.calculate(data)), 0)
+
+        writer = CsvWriter()
+        writer.save_csv(aggregated_data, 'test_file')
+        self.assertIn('test_file.csv', os.listdir(path="."))
+
+        reader = CsvReader()
+        self.assertEqual(reader.read('test_file').shape[0], 7)
+
+
+
+
+# if __name__ == "__main__":
+#     unittest.main()
